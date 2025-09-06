41d2a4e02c380bb6f5aaef2f14c0245f5e15e17a
codeBOKER <amr6000baker@gmail.com>
2025-09-06 22:04:44 +0300

    Remove venv from git tracking

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from account.serializers import UserSerializer
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class UsersList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class UserDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
